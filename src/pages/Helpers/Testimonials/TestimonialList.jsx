dde9c4c185f966310da5ddd25b9babf66770e070
Naman Goel <namagoel@cisco.com>
2024-03-03 21:27:17 +0530

    adding slider

--- src/pages/Helpers/Testimonials/TestimonialList.jsx
+++ src/pages/Helpers/Testimonials/TestimonialList.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Img, Heading } from "../../../components";
 import Testimonial from "./Testimonial";
 
 const TestimonialList = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const testimonials = [
+        {
+            textContent: "I am very satisfied with the work on the Nexus team. Good service, satisfying work results and can also increase my business turnover. Thank you so much to the Nexus team.",
+            imgSrc: "images/img_rectangle_52.png",
+            headingName: "John Richard",
+            pos: "Businessman"
+        },
+        {
+            textContent: "Since branding with the Oracle team I feel my business has a very fast and good development, everything also always goes through a process of discussion that is really valid.",
+            imgSrc: "images/img_rectangle_52_49x49.png",
+            headingName: "Andrew Smith",
+            pos: "Founder Ulala"
+        },
+        {
+            textContent: "I can say, this team is indeed extraordinarily perfect. I will do a lot of teamwork with this team next time. I do not hesitate to cooperate because the results are very satisfying.",
+            imgSrc: "images/img_rectangle_52_1.png",
+            headingName: "Steve John",
+            pos: "CEO rumahku"
+        }
+    ];
+
+    const handlePrevSlide = () => {
+        console.log("hi 1");
+        setCurrentSlide((prevSlide) => (prevSlide === 0 ? testimonials.length - 1 : prevSlide - 1));
+    };
+
+    const handleNextSlide = () => {
+        console.log("hi 2");
+        setCurrentSlide((prevSlide) => (prevSlide === testimonials.length - 1 ? 0 : prevSlide + 1));
+    };
+
     return (
         <div className="h-[525px] w-full mt-[-34px] relative">
             <Img
@@ -17,39 +49,28 @@ const TestimonialList = () => {
                     </Heading>
                     <div className="flex flex-row justify-start w-[13%] mt-0.5 gap-[9px]">
                         <div className="flex flex-row justify-start w-[48%]">
-                            <div className="flex flex-row justify-center w-full p-[11px] bg-red-400_01">
+                            <div className="flex flex-row justify-center w-full p-[11px] bg-red-400_01 btn--left" onClick={handlePrevSlide}>
                                 <Img src="images/img_frame_660.svg" alt="image_two" className="h-4 mx-[7px]" />
                             </div>
                         </div>
                         <div className="flex flex-row justify-start w-[48%]">
-                            <div className="flex flex-row justify-center w-full p-[11px] bg-red-400_01">
+                            <div className="flex flex-row justify-center w-full p-[11px] bg-red-400_01 btn--right" onClick={handleNextSlide}>
                                 <Img src="images/img_frame_661.svg" alt="image_three" className="h-4 mx-[7px]" />
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-row w-full gap-[30px]">
-                    <Testimonial 
-                        textContent="I am very satisfied with the work on the Nexus team. good service, satisfying work results and
-                          can also increase my business turnover. Thank you so much to the Nexus team." 
-                        imgSrc="images/img_rectangle_52.png" 
-                        headingName="John Richard" 
-                        pos="Businessman" 
-                    />
-                    <Testimonial 
-                        textContent="since branding with the oracle team i feel my business has a very fast and good development,
-                        everything also always goes through a process of discussion that is really valid" 
-                        imgSrc="images/img_rectangle_52_49x49.png" 
-                        headingName="Andrew Smith" 
-                        pos="Founder Ulala" 
-                    />
-                    <Testimonial 
-                        textContent="I can say, this team is indeed extraordinarily perfect. I will do a lot of teamwork with this
-                        team next time. I do not hesitate to cooperate because the results are very satisfying" 
-                        imgSrc="images/img_rectangle_52_1.png" 
-                        headingName="Steve John" 
-                        pos="CEO rumahku" 
-                    />
+                <div className="flex flex-row w-full gap-[30px] testimonials">
+                    {testimonials.map((testimonial, index) => (
+                        <Testimonial
+                            key={index}
+                            textContent={testimonial.textContent}
+                            imgSrc={testimonial.imgSrc}
+                            headingName={testimonial.headingName}
+                            pos={testimonial.pos}
+                            isActive={index === currentSlide}
+                        />
+                    ))}
                 </div>
             </div>
         </div>
